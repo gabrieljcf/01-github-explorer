1a298b47f60bc3804b2252c2b024622f3f4f0571
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-03-01 22:01:19 -0300

    feature: create react structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-import path from "path";
+const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
