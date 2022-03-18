30b72bfde7b6eee8b032c17a4015a40944b95b82
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-03-17 22:38:37 -0300

    feature: add component repository item

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,36 +1,18 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositoryItem"
+const repository = {
+  name: "unform2",
+  description: "Forms in React",
+  link: "https://github.com/gabrieljcf/01-github-explorer"
+}
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
       </ul>
     </section>
   )
